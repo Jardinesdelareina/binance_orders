0116d136b8a05cafc4454521785ea5e616a3b3d7
jardinesdelareina <jardinesdelareina@mail.ru>
2023-05-28 13:47:18 +0300

    select_queries

--- select_queries.sql
+++ select_queries.sql
@@ -25,5 +25,29 @@ SELECT commission_asset AS com_as FROM linkusdt;
 -- вывести максимальную цену btcusdt за предиод
 SELECT MAX(price) FROM btcusdt WHERE time BETWEEN '2022-01-01 00:00:00' AND '2023-01-01 00:00:00';
 
+-- вывести ордера, чья price в интервале
+SELECT order_id FROM btcusdt WHERE price BETWEEN 20000 AND 30000;
+
 -- вывести среднее округленное qty из xrpusdt
 SELECT ROUND(AVG(qty)) FROM xrpusdt;
+
+-- вывести order_id и новый столбец с результатом произведения
+SELECT order_id, (price * quote_qty) AS value FROM btcusdt; 
+
+-- вывести order_id ордеров, чьи commission_asset НЕ USDT
+SELECT order_id FROM xrpusdt WHERE commission_asset <> 'USDT';
+
+-- вывести ордера, отсортированные по time в порядке убывания
+SELECT order_id, time FROM btcusdt ORDER BY time DESC;
+
+-- вывести количество записей, сгруппированных по commission_asset 
+SELECT commission_asset, COUNT(*) FROM btcusdt GROUP BY commission_asset;
+
+-- объединенить данные из двух таблиц (UNION устраняет дубликаты, UNION ALL выводит вместе с дубликатами)
+SELECT order_id, price FROM bnbusdt UNION SELECT order_id, price FROM linkusdt;
+
+-- вывести commission_asset, которые есть в таблицах btcusdt и xrpusdt
+SELECT commission_asset FROM btcusdt INTERSECT SELECT commission_asset FROM xrpusdt;
+
+-- вывести commission_asset, которых нет в таблицах btcusdt и xrpusdt
+SELECT commission_asset FROM btcusdt EXCEPT SELECT commission_asset FROM xrpusdt;
